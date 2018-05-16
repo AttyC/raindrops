9897102b9ec35fd073de8a97d116fdc7335c7c02
Antonia Cronin <antoniajcronin@gmail.com>
2018-05-16 15:00:02 +0100

    refactor test structure for readability

--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -2,11 +2,29 @@ require './lib/raindrop.rb'
 
 describe Raindrop do
   describe '#raindrop_output' do
-    it 'returns "Pling" if input is factor of 3' do
-      expect(subject.raindrop_output(3)).to eq "Pling"
+
+    context 'when number is a factor of 3' do
+      it 'returns "Pling"' do
+        expect(subject.raindrop_output(3)).to eq "Pling"
+      end
     end
-    it 'returns "Plang" if input is factor of 5' do
-      expect(subject.raindrop_output(5)).to eq "Plang"
+
+    context 'when number is a factor of 5' do
+      it 'returns "Plang"' do
+        expect(subject.raindrop_output(5)).to eq "Plang"
+      end
+    end
+
+    context 'when number is a factor of 7' do
+      it 'returns "Plong"' do
+        expect(subject.raindrop_output(7)).to eq "Plong"
+      end
+    end
+    
+    context 'when number is a factor of combined 3/5/7' do
+      it 'returns "PlingPlong" if input is factor of 3 and 7' do
+        expect(subject.raindrop_output(21)).to eq "PlingPlong"
+      end
     end
   end
 end
