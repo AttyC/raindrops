70095ea837d61b64fad3711ff8c03b9aff4fbf0b
Antonia Cronin <antoniajcronin@gmail.com>
2018-05-17 09:02:36 +0100

    refactor raindrop class

--- lib/raindrop.rb
+++ lib/raindrop.rb
@@ -1,13 +1,24 @@
 class Raindrop
+  attr_reader :number
 
   def raindrop_output(number)
     output = ''
-    if number %3 == 0
-      output += "Pling"
-    end
-    if number %5 == 0
-      output += "Plang"
-    end
+
+    output += "Pling" if factor_of_3(number)
+    output += "Plang" if factor_of_5(number)
+    output += "Plong" if factor_of_7(number)
     output
+  end
+
+  def factor_of_3(number)
+    true if number %3 == 0
+  end
+
+  def factor_of_5(number)
+    true if number %5 == 0
+  end
+
+  def factor_of_7(number)
+    true if number %7 == 0
   end
 end
